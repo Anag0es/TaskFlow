49323e673ed3d4b7487c85af4d1354589e5222c4
Anag0es <analuisa.goes2004@gmail.com>
2024-03-20 23:59:56 -0300

    feat: model de usuario e tarefa

---
+++ TaskFlow/src/main/java/com/example/TaskFlow/model/Usuario.java
@@ -0,0 +1,16 @@
+package com.example.TaskFlow.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+}

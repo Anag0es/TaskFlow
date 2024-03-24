1b2192737cba3187ba8d01adf9c85c39b6827ca1
Anag0es <analuisa.goes2004@gmail.com>
2024-03-23 21:24:29 -0300

    feat: integracao cadastro

--- TaskFlow/src/main/frontend/src/routes.jsx
+++ TaskFlow/src/main/frontend/src/routes.jsx
@@ -3,12 +3,14 @@ import './reset.css';
 import './styles.css';
 
 import Home from './pages/Home';
+import Inscricao from './pages/Inscricao';
 
 export default function AppRoutes(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/inscricao" element={<Inscricao/>}/>
             </Routes>
         </BrowserRouter>
     )
--- TaskFlow/src/main/java/com/example/TaskFlow/model/Usuario.java
+++ TaskFlow/src/main/java/com/example/TaskFlow/model/Usuario.java
@@ -1,6 +1,7 @@
 package com.example.TaskFlow.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,8 +18,11 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String nome;
+    @NotBlank
     private String email;
+    @NotBlank
     private String senha;
 
 }

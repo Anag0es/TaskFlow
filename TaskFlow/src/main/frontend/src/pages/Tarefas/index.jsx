a80d7034bc221d85a356f7d5aa22641a0383bc8c
Anag0es <analuisa.goes2004@gmail.com>
2024-03-25 13:46:27 -0300

    feat: criacao de tarefas

--- TaskFlow/src/main/frontend/src/pages/Tarefas/index.jsx
+++ TaskFlow/src/main/frontend/src/pages/Tarefas/index.jsx
@@ -2,13 +2,19 @@ import styles from './styles.module.css';
 import React, { useState } from 'react';
 
 import SideBar from '../../component/SideBar';
+import GerenciadorTarefas from '../../component/GerenciadorDeTarefas';
 
 export default function Tarefas(){
     return(
         <div className={styles.container}>
             <SideBar/>
             <div className={styles.content}>
-                <h1>Tarefas</h1>
+                <div className={styles.tarefas}>
+                   <h1>Tarefas</h1>
+                </div>
+                <div className={styles.pendente}>
+                    <GerenciadorTarefas/>
+                </div>
             </div>
         </div>
     );

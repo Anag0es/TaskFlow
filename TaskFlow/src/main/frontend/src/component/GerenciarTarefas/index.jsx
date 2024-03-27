524d18c9996755fbbcf1608e09be661e0aafe10a
Anag0es <analuisa.goes2004@gmail.com>
2024-03-27 01:21:28 -0300

    feat: quadro de tarefas

--- TaskFlow/src/main/frontend/src/component/GerenciarTarefas/index.jsx
+++ TaskFlow/src/main/frontend/src/component/GerenciarTarefas/index.jsx
@@ -12,8 +12,8 @@ export default function GerenciarTarefas({ userId }) {
         userId: userId
     });
 
-      const [mostrarFormulario, setMostrarFormulario] = useState(false); // Estado para controlar a exibição do formulário
-
+    // Estado para controlar qual formulário mostrar
+    const [mostrarFormulario, setMostrarFormulario] = useState(null);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -23,125 +23,83 @@ export default function GerenciarTarefas({ userId }) {
     const handleSubmit = (e) => {
         e.preventDefault();
         setTarefas([...tarefas, novaTarefa]);
-        setNovaTarefa({ // Reseta o estado da nova tarefa
+        setNovaTarefa({
             titulo: '',
             descricao: '',
             dataConclusao: '',
             prioridade: 'baixa',
             status: 'pendente',
-            userId: userId // Mantém userId no estado da nova tarefa
+            userId: userId
         });
-         setMostrarFormulario(false); // Após enviar, esconder o formulário
+        setMostrarFormulario(null); // Após enviar, esconder o formulário
     };
 
-    const toggleFormulario = () => {
-            setMostrarFormulario(!mostrarFormulario); // Alternar a visibilidade do formulário
-        };
+    const toggleFormulario = (status) => {
+        setMostrarFormulario(mostrarFormulario === status ? null : status); // Alternar a visibilidade do formulário
+    };
 
-    return (
-            <div className={styles.container}>
-                <div className={styles.pendente}>
-                    <h1 className={styles.titulo}>
-                        Pendente
-                        <button onClick={toggleFormulario} className={styles.botaoMais}>+</button> {/* Botão para mostrar o formulário */}
-                    </h1>
-                    <div className={styles.boxPendente}>
-                        {tarefas.map((tarefa, index) => {
-                            if (tarefa.status === 'pendente') {
-                                return <div key={index} className={styles.quadro}>{tarefa.titulo}</div>;
-                            }
-                            return null;
-                        })}
-                    </div>
-                </div>
-                {mostrarFormulario && ( // Renderizar o formulário apenas se mostrarFormulario for verdadeiro
-                    <div className={styles.modal}>
-                        <form onSubmit={handleSubmit} className={styles.form}>
-                            <input type="text" name="titulo" placeholder="Título" value={novaTarefa.titulo} onChange={handleInputChange} />
-                            <input type="text" name="descricao" placeholder="Descrição" value={novaTarefa.descricao} onChange={handleInputChange} />
-                            <input type="date" name="dataConclusao" placeholder="Data de Conclusão" value={novaTarefa.dataConclusao} onChange={handleInputChange} />
-                            <select name="prioridade" value={novaTarefa.prioridade} onChange={handleInputChange}>
+    // Refatoração do código para a renderização do formulário baseado no estado 'mostrarFormulario'
+const renderFormularioModal = () => (
+<>
+        <div className={mostrarFormulario ? `${styles.modalBackdrop} ${styles.active}` : styles.modalBackdrop}></div>
+        <div className={mostrarFormulario ? `${styles.modal} ${styles.modalActive}` : styles.modal}>
+                <form onSubmit={handleSubmit} className={styles.form}>
+                            <input type="text" name="titulo" placeholder="Título" value={novaTarefa.titulo} onChange={handleInputChange} className={styles.inputForms} />
+                            <input type="text" name="descricao" placeholder="Descrição" value={novaTarefa.descricao} onChange={handleInputChange} className={styles.inputForms}/>
+                            <input type="date" name="dataConclusao" placeholder="Data de Conclusão" value={novaTarefa.dataConclusao} onChange={handleInputChange} className={styles.inputForms}/>
+                            <select name="prioridade" value={novaTarefa.prioridade} onChange={handleInputChange} className={styles.selectForms}>
                                 <option value="baixa">Baixa</option>
                                 <option value="média">Média</option>
                                 <option value="alta">Alta</option>
                             </select>
-                            <select name="status" value={novaTarefa.status} onChange={handleInputChange}>
+                            <select name="status" value={novaTarefa.status} onChange={handleInputChange} className={styles.selectForms}>
                                 <option value="pendente">Pendente</option>
                                 <option value="em andamento">Em Andamento</option>
                                 <option value="concluída">Concluída</option>
                             </select>
                             <button type="submit">Adicionar Tarefa</button>
                         </form>
-                    </div>
-                )}
-               <div className={styles.Andamento}>
-                  <h1 className={styles.titulo}>
-                     Em andamento
-                   <button onClick={toggleFormulario} className={styles.botaoMais}>+</button> {/* Botão para mostrar o formulário */}
-                  </h1>
-                   <div className={styles.boxAndamento}>
-                    {tarefas.map((tarefa, index) => {
-                       if (tarefa.status === 'em andamento') {
-                            return <div key={index} className={styles.quadro}>{tarefa.titulo}</div>;
-                            }
-                              return null;
-                      })}
-                    </div>
-              </div>
-                       {mostrarFormulario && ( // Renderizar o formulário apenas se mostrarFormulario for verdadeiro
-                                  <div className={styles.modal}>
-                                      <form onSubmit={handleSubmit} className={styles.form}>
-                                          <input type="text" name="titulo" placeholder="Título" value={novaTarefa.titulo} onChange={handleInputChange} />
-                                          <input type="text" name="descricao" placeholder="Descrição" value={novaTarefa.descricao} onChange={handleInputChange} />
-                                          <input type="date" name="dataConclusao" placeholder="Data de Conclusão" value={novaTarefa.dataConclusao} onChange={handleInputChange} />
-                                          <select name="prioridade" value={novaTarefa.prioridade} onChange={handleInputChange}>
-                                              <option value="baixa">Baixa</option>
-                                              <option value="média">Média</option>
-                                              <option value="alta">Alta</option>
-                                          </select>
-                                          <select name="status" value={novaTarefa.status} onChange={handleInputChange}>
-                                              <option value="pendente">Pendente</option>
-                                              <option value="em andamento">Em Andamento</option>
-                                              <option value="concluída">Concluída</option>
-                                          </select>
-                                          <button type="submit">Adicionar Tarefa</button>
-                                      </form>
-                                  </div>
-                       )}
-                        <div className={styles.concluida}>
-                                <h1 className={styles.titulo}>
-                                   Concluida
-                                 <button onClick={toggleFormulario} className={styles.botaoMais}>+</button> {/* Botão para mostrar o formulário */}
-                                </h1>
-                                 <div className={styles.boxConcluida}>
-                                  {tarefas.map((tarefa, index) => {
-                                     if (tarefa.status === 'em andamento') {
-                                          return <div key={index} className={styles.quadro}>{tarefa.titulo}</div>;
-                                          }
-                                         return null;
-                                    })}
-                                 </div>
-                        </div>
-                       {mostrarFormulario && ( // Renderizar o formulário apenas se mostrarFormulario for verdadeiro
-                                                <div className={styles.modal}>
-                                                    <form onSubmit={handleSubmit} className={styles.form}>
-                                                        <input type="text" name="titulo" placeholder="Título" value={novaTarefa.titulo} onChange={handleInputChange} />
-                                                        <input type="text" name="descricao" placeholder="Descrição" value={novaTarefa.descricao} onChange={handleInputChange} />
-                                                        <input type="date" name="dataConclusao" placeholder="Data de Conclusão" value={novaTarefa.dataConclusao} onChange={handleInputChange} />
-                                                        <select name="prioridade" value={novaTarefa.prioridade} onChange={handleInputChange}>
-                                                            <option value="baixa">Baixa</option>
-                                                            <option value="média">Média</option>
-                                                            <option value="alta">Alta</option>
-                                                        </select>
-                                                        <select name="status" value={novaTarefa.status} onChange={handleInputChange}>
-                                                            <option value="pendente">Pendente</option>
-                                                            <option value="em andamento">Em Andamento</option>
-                                                            <option value="concluída">Concluída</option>
-                                                        </select>
-                                                        <button type="submit">Adicionar Tarefa</button>
-                                                    </form>
-                                                </div>
-                                     )}
+        </div>
+    </>
+);
+
+    return (
+        <div className={styles.container}>
+        <h1 className={styles.titulo}>
+             Pendente
+            <button onClick={() => toggleFormulario('pendente')} className={styles.botaoMais}>+</button>
+            </h1>
+            <div className={styles.tarefas}>
+            <div className={styles.boxPendente}>
+                {tarefas.filter(tarefa => tarefa.status === 'pendente').map((tarefa, index) => (
+                    <div key={index}>{tarefa.titulo}</div>
+                ))}
             </div>
-        );
+            {mostrarFormulario === 'pendente' && renderFormularioModal()}
+
+            <h1 className={styles.titulo}>
+             Em andamento
+            <button onClick={() => toggleFormulario('andamento')} className={styles.botaoMais}>+</button>
+            </h1>
+            <div className={styles.boxAndamento}>
+                {tarefas.filter(tarefa => tarefa.status === 'andamento').map((tarefa, index) => (
+                    <div key={index}>{tarefa.titulo}</div>
+                ))}
+            </div>
+            {mostrarFormulario === 'andamento' && renderFormularioModal()}
+
+            <h1 className={styles.titulo}>
+             Concluído
+            <button onClick={() => toggleFormulario('concluido')} className={styles.botaoMais}>+</button>
+            </h1>
+            <div className={styles.boxConcluida}>
+                {tarefas.filter(tarefa => tarefa.status === 'concluido').map((tarefa, index) => (
+                    <div key={index}>{tarefa.titulo}</div>
+                ))}
+            </div>
+            {mostrarFormulario === 'concluido' && renderFormularioModal()}
+        </div>
+        </div>
+    );
 }
+

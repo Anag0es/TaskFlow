c14cf3a11f67143c9c98f7daaa4f2e6066c7e9fc
Anag0es <analuisa.goes2004@gmail.com>
2024-04-04 22:16:52 -0300

    fix: classes 'enum'

--- TaskFlow/src/main/java/com/example/TaskFlow/model/PrioridadeEnum.java
+++ TaskFlow/src/main/java/com/example/TaskFlow/model/PrioridadeEnum.java
@@ -1,7 +1,15 @@
 package com.example.TaskFlow.model;
 
 public enum PrioridadeEnum {
-    BAIXA,
-    MEDIA,
-    ALTA
+    BAIXA, MEDIA, ALTA;
+
+    public static PrioridadeEnum getEnum(String value) {
+        for (PrioridadeEnum v : values()) {
+            if (v.name().equalsIgnoreCase(value)) {
+                return v;
+            }
+        }
+        throw new IllegalArgumentException("Prioridade inválida: " + value);
+    }
 }
+
--- TaskFlow/src/main/java/com/example/TaskFlow/model/StatusEnum.java
+++ TaskFlow/src/main/java/com/example/TaskFlow/model/StatusEnum.java
@@ -1,8 +1,15 @@
 package com.example.TaskFlow.model;
 
 public enum StatusEnum {
-    PENDENTE,
-    EM_ANDAMENTO,
-    CONCLUIDA,
+    PENDENTE, EM_ANDAMENTO, CONCLUIDA;
+
+    public static StatusEnum getEnum(String value) {
+        for (StatusEnum v : values()) {
+            if (v.name().equalsIgnoreCase(value)) {
+                return v;
+            }
+        }
+        throw new IllegalArgumentException("Status inválida: " + value);
+    }
 
 }
